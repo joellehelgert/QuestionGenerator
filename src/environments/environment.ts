04efbe4d68785d81c2996c8e8727efb0ad5f75d0
Joelle Helgert <joelle.helgert@gmail.com>
2020-04-29 11:20:32 +0200

    Cofniguring Firebase with Angular and add Firebase Instructions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAbNjv3ixaawMTjvnKyR_dAYWvFD-EjJlE',
+    authDomain: 'adorableapi.firebaseapp.com',
+    databaseURL: 'https://adorableapi.firebaseio.com',
+    projectId: 'adorableapi',
+    storageBucket: 'adorableapi.appspot.com',
+    messagingSenderId: '148611555098',
+    appId: '1:148611555098:web:a8b7736da2ae0c2ddf56a3'
+  }
 };
 
 /*
