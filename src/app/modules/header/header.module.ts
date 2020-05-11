1c2ef3b4aeccee39b42cbf35a68272646f305dbb
JuliaGruber <ju.gruber@gmx.at>
2020-05-11 08:39:45 +0200

    include header and footer

---
+++ src/app/modules/header/header.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent],
+  exports: [HeaderComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ]
+})
+export class HeaderModule { }
