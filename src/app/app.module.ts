5c65993f39badf489f8cd18a9bd8d965922a4408
joelle.helgert <joelle.helgert@gmail.com>
2020-05-29 00:34:28 +0200

    adding ngxs to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
@@ -12,7 +13,10 @@ import { environment } from '../environments/environment';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { HttpClientModule } from '@angular/common/http';
-import {AuthModule} from "./modules/login/auth.module";
+import {AuthModule} from './modules/login/auth.module';
+import { HintState } from './states/HintState';
+import { HintComponent } from './modules/hint/components/hint.component';
+import { HintModule } from './modules/hint/hint.module';
 
 @NgModule({
   declarations: [
@@ -22,6 +26,7 @@ import {AuthModule} from "./modules/login/auth.module";
     BrowserModule,
     AuthModule,
     AppRoutingModule,
+    NgxsModule.forRoot([HintState], {}),
     HeaderModule,
     FooterModule,
     QuestionnaireModule,
@@ -29,9 +34,10 @@ import {AuthModule} from "./modules/login/auth.module";
     AngularFirestoreModule,
     AngularFireAuthModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    HintModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HintComponent]
 })
 export class AppModule { }
