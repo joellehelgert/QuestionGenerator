106246ae195d5e8c6c36fc39d8b62c2f1754bf11
joelle.helgert <joelle.helgert@gmail.com>
2020-05-29 14:36:27 +0200

    added error and success messages

--- src/app/modules/hint/components/hint/hint.component.ts
+++ src/app/modules/hint/components/hint/hint.component.ts
@@ -1,24 +1,18 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
-import { Store } from '@ngxs/store';
+import { Store, Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { SimpleErrorObject } from 'src/app/states/HintState';
+import { SimpleErrorObject, HintState } from 'src/app/states/HintState';
 
 @Component({
   selector: 'app-hint',
   templateUrl: './hint.component.html',
   styleUrls: ['./hint.component.scss']
 })
-export class HintComponent implements OnInit, OnChanges {
-  errors$: Observable<SimpleErrorObject[]>;
-  successes$: Observable<string[]>;
+export class HintComponent implements OnInit {
+  @Select(HintState.getErrors) errors$: Observable<SimpleErrorObject[]>;
+  @Select(HintState.getSuccesses) successes$: Observable<string[]>;
 
-  constructor(private store: Store) {
-    this.errors$ = this.store.select(state => state.status.errors);
-    this.successes$ = this.store.select(state => state.status.successes);
-   }
+  constructor(private store: Store) {}
 
   ngOnInit(): void {}
-
-  ngOnChanges() {}
-
 }
